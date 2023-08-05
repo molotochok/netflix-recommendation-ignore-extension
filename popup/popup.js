ac74da70ed0b0105fd341f86e5db2c7bcf7aeae2
Markiian Karpa <markian98@gmail.com>
2023-08-05 16:30:53 +0200

    Fix cache key in popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -5,16 +5,16 @@ import { IGNORE_ENABLED_KEY } from '../scripts/cacheKeys.js';
   const $checkbox = $switch.querySelector("#switchInput");
 
   $checkbox.addEventListener('change', (event) => {
-    chrome.storage.local.set({ [STORAGE_KEY]: event.currentTarget.checked});
+    chrome.storage.local.set({ [IGNORE_ENABLED_KEY]: event.currentTarget.checked});
   });
 
   const cache = await chrome.storage.local.get(IGNORE_ENABLED_KEY);
   
-  if (cache && cache[STORAGE_KEY] !== undefined) {
-    $checkbox.checked = cache[STORAGE_KEY];
+  if (cache && cache[IGNORE_ENABLED_KEY] !== undefined) {
+    $checkbox.checked = cache[IGNORE_ENABLED_KEY];
   } else {
     $checkbox.checked = true;
-    chrome.storage.local.set({ [STORAGE_KEY]: true});
+    chrome.storage.local.set({ [IGNORE_ENABLED_KEY]: true});
   } 
 
   document.body.appendChild($switch);
